2c0c4cf5f8947e979c3e6570ef3a775112603d51
kavitchugh16 <kavitchugh@gmail.com>
2025-09-26 17:41:39 +0530

    loader

---
+++ src/components/common/Loader.jsx
@@ -0,0 +1,13 @@
+// client/src/components/common/Loader.jsx
+
+import React from 'react';
+
+const Loader = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-indigo-600"></div>
+    </div>
+  );
+};
+
+export default Loader;
